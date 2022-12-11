5f79729fe85147db57c1a44326a1b9921b2bf257
minyoung <yakc0202@gmail.com>
2022-12-11 17:12:23 +0900

    six

--- devBoard-front/src/AppConfig.js
+++ devBoard-front/src/AppConfig.js
@@ -2,7 +2,7 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-backendHost = "http://3.18.52.170:8090";
+backendHost = "http://172.18.0.3:8090";
 
 
 export const API_BASE_URL = `${backendHost}`;
